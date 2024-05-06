00f409d1c03ef544f9e1ef8627d039de207b13ac
Michael Busch <b.michael.2002@web.de>
2024-05-06 14:06:46 +0200

    Caeaser Chiffre gefixed und Caeser cracker implementiert

--- Praktikum4/src/CaesarCipher.java
+++ Praktikum4/src/CaesarCipher.java
@@ -1,26 +1,50 @@
 public class CaesarCipher extends AbstractMonoalphabeticalCipher implements IsCrackable{
-    private char[] key;
+    private char key;
 
-    CaesarCipher(char[] key, char[] text){
+    CaesarCipher(char key, char[] text){
         super(text);
         this.key = key;
     }
 
-    public char[] getKey() {
+    public char getKey() {
         return key;
     }
 
-    public void setKey(char[] key) {
+    public void setKey(char key) {
         this.key = key;
     }
 
     @Override
     public char[] encode() {
-        return null;
+        char[] encodedText = new char[getText().length];
+        for (int i = 0; i < getText().length; i++) {
+            if(getText()[i] == ' '){
+                encodedText[i] = ' ';
+                continue;
+            }
+
+            int numberToAdd = (getText()[i]) - (Character.isUpperCase(getText()[i]) ? 65 : 97) + (Character.isLowerCase(key) ? key - 97 : key - 65);
+            encodedText[i] = (char) ((numberToAdd % 26) + (Character.isUpperCase(getText()[i]) ? 65 : 97));
+        }
+        return encodedText;
     }
 
     @Override
     public char[] decode() {
-        return null;
+        char[] decodedText = new char[getText().length];
+        for (int i = 0; i < getText().length; i++) {
+            if(getText()[i] == ' '){
+                decodedText[i] = ' ';
+                continue;
+            }
+
+            int number = (getText()[i] - (Character.isLowerCase(getText()[i]) ? 97 : 65) - (Character.isLowerCase(key) ? key - 97 : key - 65));
+            if(number < 0){
+                decodedText[i] = (char) (number + (Character.isLowerCase(key) ? 123 : 91));
+            }else {
+                decodedText[i] = (char) (number + (Character.isLowerCase(key) ? 97 : 65));
+            }
+        }
+        return decodedText;
     }
 }
