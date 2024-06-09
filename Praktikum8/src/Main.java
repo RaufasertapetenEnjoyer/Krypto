6b37acc58fecbcabf46e7cb4ec3a685e2c5d8a4e
Jan Treiber <jntreiber@gmail.com>
2024-06-09 20:50:32 +0200

    Praktikum 8 fertig alle Tests durchlaufen, gerne nochmal überprüfen

--- Praktikum8/src/Main.java
+++ Praktikum8/src/Main.java
@@ -2,7 +2,12 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Polynomial test = new Polynomial(Arrays.asList(1, 1, -5, 6, 7, 10));
-        System.out.println(test);
+        Polynomial test1 = new Polynomial(Arrays.asList(-20, -2 , 5, 7, 10));
+        Polynomial test2 = new Polynomial(Arrays.asList(2, 6 , 3));
+        test1.divide(test2);
+
+        Polynomial p1 = new Polynomial(Arrays.asList(0, 0, -12, 1)); // x^3 - 12x + 0x^2 + 0
+        Polynomial p2 = new Polynomial(Arrays.asList(-10, 1)); // -10 + x
+        p1.divide(p2);
     }
 }
--- Praktikum8/src/Polynomial.java
+++ Praktikum8/src/Polynomial.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.List;
 
 public class Polynomial {
@@ -8,8 +9,43 @@ public class Polynomial {
         this.polynom = polynom;
     }
 
-    public Polynomial[] divide(Polynomial divisor) {
-        return null;
+    /**
+     * method for polynomial division
+     *
+     * @param divisor Polynom that should divide the current Polynom
+     * @return Array of Polynoms, index 0 is the result and index 1 the rest
+     * @throws IllegalArgumentException division by zero or greater divisor throws this exception
+     */
+    public Polynomial[] divide(Polynomial divisor) throws IllegalArgumentException{
+        List<Integer> divisorPolynom = divisor.polynom;
+        int divisorDegree = divisorPolynom.size()-1;
+        List<Integer> dividendPolynom = this.polynom;
+        int dividendDegree = dividendPolynom.size()-1;
+        boolean isZero = true;
+        for (Integer factor : divisorPolynom) {
+            if (factor != 0) {
+                isZero = false;
+                break;
+            }
+        }
+        if (divisorDegree > dividendDegree || (divisorDegree == dividendDegree && divisorPolynom.get(divisorDegree) > dividendPolynom.get(dividendDegree)) ||isZero) {
+            throw new IllegalArgumentException();
+        }
+
+        int resultDegree = dividendDegree - divisorDegree;
+        Integer[] integerPolynomArr = new Integer[resultDegree+1];
+
+        System.out.println("start");
+        for(int i = dividendDegree; i >= dividendDegree-resultDegree; i--) {
+            int a = Math.round((float)dividendPolynom.get(i) / divisorPolynom.get(divisorDegree));
+            integerPolynomArr[i-divisorDegree] = a;
+            int m = divisorDegree;
+            for(int j = i; j >= i-divisorDegree; j--) {
+                dividendPolynom.set(j, dividendPolynom.get(j) - a * divisorPolynom.get(m));
+                m--;
+            }
+        }
+        return new Polynomial[]{new Polynomial(Arrays.asList(integerPolynomArr)), new Polynomial(dividendPolynom)};
     }
 
     public String toString() {
@@ -17,21 +53,22 @@ public class Polynomial {
         for (int i = polynom.size()-1; i >= 0; i--) {
             if (polynom.get(i) != 0) {
                 builder.append(!builder.isEmpty() ? " + " : "");
-                if (polynom.get(i) == -1) {
-                    builder.append("-");
-                } else if (polynom.get(i) != 1 || i == 0) {
-                    builder.append(polynom.get(i));
-                }
+                /* hübschere Ausgabe ohne 1*/
+//                if (polynom.get(i) == -1) {
+//                    builder.append("-");
+//                } else if (polynom.get(i) != 1 || i == 0) {
+//                    builder.append(polynom.get(i));
+//                }
+                builder.append(polynom.get(i));
                 if (i != 0 ) {
                     builder.append("x");
-                    if (i > 1) {
-                        builder.append("^" + i);
-                    }
+//                    builder.append(i > 1 ? ("^" + i) : ""); // hübschere Ausgabe
+                    builder.append("^" + i);
                 }
             }
         }
         String result = builder.toString();
-        return result.isEmpty() ? "0" : result;
+        return result.isEmpty() ? "" : result;
     }
 
 
